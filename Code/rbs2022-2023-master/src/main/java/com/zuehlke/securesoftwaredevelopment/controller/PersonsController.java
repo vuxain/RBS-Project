023496ed039df30a8c95638816baba57e2ba4a26
Vukašin Radić <vukasinradic96@hotmail.com>
2023-04-19 22:13:46 +0200

    Added Authorization

--- Code/rbs2022-2023-master/src/main/java/com/zuehlke/securesoftwaredevelopment/controller/PersonsController.java
+++ Code/rbs2022-2023-master/src/main/java/com/zuehlke/securesoftwaredevelopment/controller/PersonsController.java
@@ -1,6 +1,7 @@
 package com.zuehlke.securesoftwaredevelopment.controller;
 
 import com.zuehlke.securesoftwaredevelopment.config.AuditLogger;
+import com.zuehlke.securesoftwaredevelopment.config.SecurityUtil;
 import com.zuehlke.securesoftwaredevelopment.domain.Person;
 import com.zuehlke.securesoftwaredevelopment.domain.User;
 import com.zuehlke.securesoftwaredevelopment.repository.PersonRepository;
@@ -9,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -34,6 +36,7 @@ public class PersonsController {
     }
 
     @GetMapping("/persons/{id}")
+    @PreAuthorize("hasAuthority('VIEW_PERSON')")
     public String person(@PathVariable int id, Model model, HttpSession session) {
         String csrf = session.getAttribute("CSRF_TOKEN").toString();
         model.addAttribute("CSRF_TOKEN", session.getAttribute("CSRF_TOKEN"));
@@ -65,10 +68,12 @@ public class PersonsController {
             throw new AccessDeniedException("Zabranjen pristup");
         }
         personRepository.update(person);
-        return "redirect:/persons/" + person.getId();
+        //System.out.println(SecurityUtil.getCurrentUser().getAuthorities().toString());
+        return "redirect:/movies/";
     }
 
     @GetMapping("/persons")
+    @PreAuthorize("hasAuthority('VIEW_PERSONS_LIST')")
     public String persons(Model model) {
         model.addAttribute("persons", personRepository.getAll());
         return "persons";
